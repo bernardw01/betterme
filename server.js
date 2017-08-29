0bb1fc0d6657aa9c6cd022f6e4a36b51414711e3
bernardw01 <bernard.l.williams@live.com>
2017-08-29 19:01:45 -0400

    Updated code to accommodate for total score tracking.

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var bodyParser = require("body-parser");
 var methodOverride = require("method-override");
 var exphbs = require("express-handlebars");
 var db = require("./models");
-
+var path = require("path");
 var app = express();
 var port = normalizePort(process.env.PORT || '3020');
 
